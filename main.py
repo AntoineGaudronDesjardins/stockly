247857d3fb6f5aad59651eeb4d88c817501cb025
AntoineGaudronDesjardins <106777191+AntoineGaudronDesjardins@users.noreply.github.com>
2025-04-28 17:39:46 +0200

    use set instead of dict

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ position = 0
 distances = dict()
 distances[position] = 0
 
-def find_closest(next: dict, distances: dict):
+def find_closest(next: set, distances: dict):
     imin, min = None, None
     for i in next:
         if imin is None or min is None:
@@ -15,26 +15,25 @@ def find_closest(next: dict, distances: dict):
     return imin
 
 
-next = dict()
-next[position+1] = position
+next = set()
+next.add(position+1)
 distances[position+1] = distances[position]+1
 if a[position] not in distances:
-    next[a[position]] = position
+    next.add(a[position])
     distances[a[position]] = distances[position]+1
-
 
 
 while len(next) != 0:
     position = find_closest(next, distances)
-    frompos = next.pop(position)
+    next.remove(position)
     if position+1 < n and position+1 not in next and position+1 not in distances:
-        next[position+1] = position
+        next.add(position+1)
         distances[position+1] = distances[position]+1
     if position-1 > 0 and position-1 not in next and position-1 not in distances:
-        next[position-1] = position
+        next.add(position-1)
         distances[position-1] = distances[position]+1
     if a[position] not in next and a[position] not in distances:
-        next[a[position]] = position
+        next.add(a[position])
         distances[a[position]] = distances[position]+1
 
 for i in range(n):
