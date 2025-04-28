57be6545c1eb2009d370a34d4fcb9509b526a65d
AntoineGaudronDesjardins <106777191+AntoineGaudronDesjardins@users.noreply.github.com>
2025-04-28 17:53:55 +0200

    use ordered list instead of set

--- main.py
+++ main.py
@@ -5,36 +5,37 @@ position = 0
 distances = dict()
 distances[position] = 0
 
-def find_closest(next: set, distances: dict):
-    imin, min = None, None
-    for i in next:
-        if imin is None or min is None:
-            imin, min = i, distances[i]
-        elif distances[i] < min:
-            imin, min = i, distances[i]
-    return imin
+
+def ordered_insert(array: list, node: int, distances: dict):
+    l = len(array)
+    for i in range(l):
+        if distances[array[l-1-i]] < distances[node]:
+            array.insert(l-i, node)
+            break
+    else:
+        array.insert(0, node)
+    return array
 
 
-next = set()
-next.add(position+1)
+next = []
+next.append(position+1)
 distances[position+1] = distances[position]+1
 if a[position] not in distances:
-    next.add(a[position])
+    next.append(a[position])
     distances[a[position]] = distances[position]+1
 
 
 while len(next) != 0:
-    position = find_closest(next, distances)
-    next.remove(position)
-    if position+1 < n and position+1 not in next and position+1 not in distances:
-        next.add(position+1)
+    position = next.pop(0)
+    if position+1 < n and position+1 not in distances:
         distances[position+1] = distances[position]+1
-    if position-1 > 0 and position-1 not in next and position-1 not in distances:
-        next.add(position-1)
+        next = ordered_insert(next, position+1, distances)
+    if position-1 > 0 and position-1 not in distances:
         distances[position-1] = distances[position]+1
-    if a[position] not in next and a[position] not in distances:
-        next.add(a[position])
+        next = ordered_insert(next, position-1, distances)
+    if a[position] not in distances:
         distances[a[position]] = distances[position]+1
+        next = ordered_insert(next, a[position], distances)
 
 for i in range(n):
     print(distances[i], end=" ")
